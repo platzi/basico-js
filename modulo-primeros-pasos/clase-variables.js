67a7ebab8c62e4d781a429178a5827cf1db46a67
Estefany Aguilar <tefy.1@live.com>
2024-07-17 11:58:46 -0500

    Add files via upload

---
+++ modulo-primeros-pasos/clase-variables.js
@@ -0,0 +1,12 @@
+let cajaDeAndy = 'Woody'
+console.log(cajaDeAndy)
+
+// Lo NO permitido
+let c = 'Woody'
+let cda = 'Woody'
+let pcAndy = 'Woody'
+
+// Lo permitido
+let primerTrasteoDeAndy = 'Woody'
+let urlDelUsuario = 'https://www.google.com'
+let idDelUsuario = '123456789'
