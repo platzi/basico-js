dc69f5ce7be6fcd768bb667e41583ffd95aed2c1
Estefany Aguilar <tefy.1@live.com>
2024-07-17 12:07:25 -0500

    Add files via upload

---
+++ modulo-arrays/clase-slice.js
@@ -0,0 +1,10 @@
+// slice()
+
+const animals = ['ant', 'bison', 'camel', 'duck', 'elephant']
+
+console.log(animals.slice(2))
+console.log(animals.slice(2, 4))
+console.log(animals.slice(1, 6))
+console.log(animals.slice(-2))
+console.log(animals.slice(2, -1))
+console.log(animals.slice())
